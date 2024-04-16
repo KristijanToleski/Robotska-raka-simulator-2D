cc4e05d8d89745c8ee173c789ff4251d68c52b40
Kristijan Toleski <kristijantoleski520@gmail.com>
2024-04-16 23:57:03 +0200

    servo 1 i 2 pomestuvanje na agol

---
+++ script.js
@@ -0,0 +1,63 @@
+let servo1 = document.getElementsByClassName("servo1")[0];
+let servo2 = document.getElementsByClassName("servo2")[0];
+
+// SERVO 1 AGOL MANUAL
+
+let slider1 = document.getElementById("servo1Agol");
+let output1 = document.getElementById("agol1");
+output1.value = slider1.value;
+
+slider1.oninput = function() {
+  output1.value = this.value;
+
+  servo1.style.transform = "rotate(" + (90 - this.value) + "deg)";
+}
+output1.oninput = function() {
+    if(this.value < 0) this.value = 0;
+    if(this.value > 90) this.value = 90;
+
+    slider1.value = this.value;
+  
+    servo1.style.transform = "rotate(" + (90 - this.value) + "deg)";
+}
+
+// SERVO 2 AGOL MANUAL
+
+let slider2 = document.getElementById("servo2Agol");
+let output2 = document.getElementById("agol2");
+output2.value = slider2.value;
+
+slider2.oninput = function() {
+  output2.value = this.value;
+
+  servo2.style.transform = "rotate(" + (180 - this.value) + "deg)";
+}
+output2.oninput = function() {
+    if(this.value < 0) this.value = 0;
+    if(this.value > 180) this.value = 180;
+
+    slider2.value = this.value;
+  
+    servo2.style.transform = "rotate(" + (180 - this.value) + "deg)";
+}
+
+// SERVO 1 I 2 AGOL AUTO
+
+let horizontalnoDvizenjeSlider = document.getElementById("horizontalnoDvizenjeSlider");
+let horizontalnoDvizenjeNumber = document.getElementById("horizontalnoDvizenjeNumber");
+
+horizontalnoDvizenjeSlider.oninput = function(){
+    horizontalnoDvizenjeNumber.value = this.value;
+    zadviziHoriznotalno(this.value);
+}
+horizontalnoDvizenjeNumber.oninput = function(){
+    if(this.value < 0) this.value = 0;
+    if(this.value > 180) this.value = 180;
+
+    horizontalnoDvizenjeSlider.value = this.value;
+    zadviziHoriznotalno(this.value);
+}
+
+function zadviziHoriznotalno(rastojanie){
+    
+}
